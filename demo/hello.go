80f6eeb43b4ff38674dd0b2b511138c22f9303a8
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-15 19:14:37 +0200

    Demo: add a demo with recipe commands

---
+++ demo/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"log"
+	"fmt"
+	"net/http"
+)
+
+func HandleIndex(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello, world\n")
+}
+
+func main() {
+
+  fmt.Println("Starting....")
+
+	// public views
+	http.HandleFunc("/", HandleIndex)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
