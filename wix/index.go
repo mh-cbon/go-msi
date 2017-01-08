253b7c9c5508b5d53808a9608772b17a6f5176bb
mh-cbon <mh-cbon@users.noreply.github.com>
2017-01-08 19:11:10 +0100

    doc and lint

--- wix/index.go
+++ wix/index.go
@@ -8,7 +8,9 @@ import (
 	"github.com/mh-cbon/go-msi/manifest"
 )
 
-// Generate required command lines to produce an msi package,
+var eol = "\r\n"
+
+// GenerateCmd generates required command lines to produce an msi package,
 func GenerateCmd(wixFile *manifest.WixManifest, templates []string, msiOutFile string, arch string) string {
 
 	cmd := ""
@@ -19,7 +21,7 @@ func GenerateCmd(wixFile *manifest.WixManifest, templates []string, msiOutFile s
 		cmd += " -gg -g1 -srd -sfrag -template fragment -dr APPDIR" + sI
 		cmd += " -var var.SourceDir" + sI
 		cmd += " -out AppFiles" + sI + ".wxs"
-		cmd += "\r\n"
+		cmd += eol
 	}
 	cmd += "candle"
 	if arch != "" {
@@ -34,24 +36,24 @@ func GenerateCmd(wixFile *manifest.WixManifest, templates []string, msiOutFile s
 		sI := strconv.Itoa(i)
 		cmd += " -dSourceDir" + sI + "=" + dir
 	}
-	for i, _ := range wixFile.Directories {
+	for i := range wixFile.Directories {
 		sI := strconv.Itoa(i)
 		cmd += " AppFiles" + sI + ".wxs"
 	}
 	for _, tpl := range templates {
 		cmd += " " + filepath.Base(tpl)
 	}
-	cmd += "\r\n"
+	cmd += eol
 	cmd += "light -ext WixUIExtension -ext WixUtilExtension -sacl -spdb "
 	cmd += " -out " + msiOutFile
-	for i, _ := range wixFile.Directories {
+	for i := range wixFile.Directories {
 		sI := strconv.Itoa(i)
 		cmd += " AppFiles" + sI + ".wixobj"
 	}
 	for _, tpl := range templates {
 		cmd += " " + strings.Replace(filepath.Base(tpl), ".wxs", ".wixobj", -1)
 	}
-	cmd += "\r\n"
+	cmd += eol
 
 	return cmd
 }
