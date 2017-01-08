e9522ba3d7645a82eaad851e1e09466a3bdcc9da
mh-cbon <mh-cbon@users.noreply.github.com>
2017-01-08 18:56:58 +0100

    doc and lint

--- guid/index.go
+++ guid/index.go
@@ -9,9 +9,10 @@ import (
 	"github.com/mh-cbon/go-msi/util"
 )
 
+// Make generates an uuid compatible with msi requirements.
 func Make() (string, error) {
-	if runtime.GOOS == "windows" {
 
+	if runtime.GOOS == "windows" {
 		b, err := util.GetBinPath()
 		if err != nil {
 			return "", err
@@ -27,16 +28,16 @@ func Make() (string, error) {
 		sout = strings.TrimSpace(sout)
 		sout = strings.ToUpper(sout)
 		return sout, nil
-	} else {
-		cmd := "uuidgen"
-		args := []string{"-r"}
-		out, err := exec.Command(cmd, args...).CombinedOutput()
-		if err != nil {
-			return "", err
-		}
-		sout := string(out)
-		sout = strings.TrimSpace(sout)
-		sout = strings.ToUpper(sout)
-		return sout, nil
 	}
+
+	cmd := "uuidgen"
+	args := []string{"-r"}
+	out, err := exec.Command(cmd, args...).CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	sout := string(out)
+	sout = strings.TrimSpace(sout)
+	sout = strings.ToUpper(sout)
+	return sout, nil
 }
