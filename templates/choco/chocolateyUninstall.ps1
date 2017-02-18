d6352ab479211f4c019dd635645cc5c664deca16
mh-cbon <mh-cbon@users.noreply.github.com>
2017-02-18 16:34:43 +0100

    choco(minor): close #5 updated uninstaller script

--- templates/choco/chocolateyUninstall.ps1
+++ templates/choco/chocolateyUninstall.ps1
@@ -1,14 +1,6 @@
 $packageName = "{{.Choco.ID}}";
 $fileType = 'msi';
-$silentArgs = '/qr /norestart';
-$validExitCodes = @(0);
 $scriptPath =  $(Split-Path $MyInvocation.MyCommand.Path);
 $fileFullPath = Join-Path $scriptPath '{{.Choco.MsiFile}}';
 
-try {
-	$msiArgs = "/x $fileFullPath $silentArgs";
-	Start-ChocolateyProcessAsAdmin "$msiArgs" 'msiexec' -validExitCodes $validExitCodes
-}
-catch {
-	throw $_.Exception
-}
+Uninstall-ChocolateyPackage $packageName $fileType "$fileFullPath /qr"
