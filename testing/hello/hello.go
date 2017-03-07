d48478db13f3dbc1f04e42a5651ba1a2c033bf78
mh-cbon <mh-cbon@users.noreply.github.com>
2017-03-07 15:50:52 +0100

    demo: add support for service

--- testing/hello/hello.go
+++ testing/hello/hello.go
@@ -5,18 +5,51 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/kardianos/service"
 )
 
-func HandleIndex(w http.ResponseWriter, r *http.Request) {
+var logger service.Logger
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "HelloSvc",
+		DisplayName: "The Hello service",
+		Description: "This is an example Go service.",
+	}
+	runAsService(svcConfig, func() {
+		fmt.Println("Starting....")
+		http.HandleFunc("/", handleIndex)
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	})
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello, world\n")
 }
 
-func main() {
+func runAsService(svcConfig *service.Config, run func()) error {
+	s, err := service.New(&program{exec: run}, svcConfig)
+	if err != nil {
+		return err
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		return err
+	}
+	return s.Run()
+}
 
-	fmt.Println("Starting....")
+type program struct {
+	exec func()
+}
 
-	// public views
-	http.HandleFunc("/", HandleIndex)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.exec()
+	return nil
+}
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
 }
